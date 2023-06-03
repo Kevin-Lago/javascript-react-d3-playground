c7ebdbfa60bae6b3cca2dd0b3662838bd11f58f5
Kevin Lago <kevinthelago@gmail.com>
2023-06-03 17:11:18 -0400

    Routes and Start Of Histogram

---
+++ src/d3/Histogram.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Histogram extends React {
+    constructor(props) {
+
+    }
+
+    render() {
+        return (
+            <div className='d3-histogram'>
+
+            </div>
+        )
+    }
+}
